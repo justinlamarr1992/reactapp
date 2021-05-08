0223e3741ff37db398a00dcc0bfff26a62871a69
justinlamarr1992 <58798470+justinlamarr1992@users.noreply.github.com>
2021-05-07 23:06:43 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+import logo from "./images/logo.svg";
+import mockup from "./images/illustration-mockups.svg"
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <img src= {logo}  alt="logo"/>
       </header>
-    </div>
+      <main>
+        <img src={mockup}  alt="mockup"/>
+        <div className="content">
+          <h2>Build The Community your fans will love</h2>
+          <p>There are also designs for the mobile view and active states in the implementation of the design as well</p>
+          <button>Register</button>
+        </div>
+        
+      </main>
+
+      <footer>
+<i class="fab fa-facebook-f"></i>
+      </footer>
+    </>
   );
 }
 
